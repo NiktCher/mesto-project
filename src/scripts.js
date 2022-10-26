9b6f4b00168257c1e34de2f6da5aa655234f1a34
NiktCher <nitchersov@yandex.ru>
2022-10-26 18:52:38 +0300

    fixes

---
+++ src/scripts.js
@@ -0,0 +1,6 @@
+import './pages/index.css';
+import './components/index.js.js';
+import './components/cards.js.js';
+import './components/api.js.js';
+import './components/modal.js.js';
+import './components/validate.js';
